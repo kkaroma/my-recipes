2b67765eed2fe40f4ec3003f382f50c2929761c2
Kaizilege Karoma <kkaroma@gmail.com>
2019-04-23 21:16:02 +0300

    Add ingredient controller

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,30 @@
+class IngredientsController < ApplicationController
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def index
+        @ingredients = Ingredient.paginate(page: params[:page], per_page: 5)
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+end
