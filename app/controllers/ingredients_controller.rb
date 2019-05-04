676227c5c570ac33fc45ccf849822f30bea7930a
Kaizilege Karoma <kkaroma@gmail.com>
2019-05-04 14:10:10 +0300

    Complete ingredients resource

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,14 +1,24 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit,:update ]
+  before_action :require_admin, except: [:show, :index]
     def new
-
+      @ingredient = Ingredient.new
     end
 
     def create
-
+      @ingredient = Ingredient.new(ingredient_params)
+      if @ingredient.save
+        flash[:success] = "Ingredient successfully created"
+        redirect_to @ingredient
+      else
+        flash[:error] = "Something went wrong"
+        render 'new'
+      end
     end
+    
 
     def show
-
+      @ingredient_recipes = @ingredient.recipes.paginate(page: params[:page], per_page: 5)
     end
 
     def index
@@ -16,15 +26,33 @@ class IngredientsController < ApplicationController
     end
 
     def edit
-
     end
 
     def update
-
+      if @ingredient.update(ingredient_params)
+        flash[:success] = "Ingredient Successfully updated"
+        redirect_to @ingredient
+      else
+        render 'edit'
+      end
     end
 
-    def destroy
 
+    private
+      def ingredient_params
+        params.require(:ingredient).permit(:name)
+      end
+
+
+    def set_ingredient
+      @ingredient = Ingredient.find(params[:id])
+    end
+
+    def require_admin
+      if !logged_in? || (logged_in? && !current_chef.admin?)
+        flash[:notice] = "You cannot perform that action"
+        redirect_to ingredients_path
+      end
     end
 
 end
