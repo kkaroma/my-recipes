e5658d1902f094b8b0f1010443ce43c36458a10e
Kaizilege Karoma <kkaroma@gmail.com>
2019-05-09 20:12:34 +0300

    Complete comment feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,10 @@ class RecipesController < ApplicationController
 	end
 
 	def show
-		@comments = Recipe.comments.paginate(page: params[:page], per_page: 5)
+		@comment = Comment.new
+		@comments = @recipe.comments.paginate(page: params[:page], per_page: 5)
+	
+
 	end
 
 	def new
